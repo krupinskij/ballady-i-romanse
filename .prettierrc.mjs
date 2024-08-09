5ec03575cdd0da72847399c7076aa17929bfc3d4
krupinskij <jk.krupinski@gmail.com>
2024-08-09 23:28:56 +0200

    Install prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,11 +1,18 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro"],
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  importOrder: ['<THIRD_PARTY_MODULES>', '^(components|context|hooks|model|utilities)/?', '^[./]'],
+  importOrderSeparation: true,
+  plugins: ['prettier-plugin-astro', '@trivago/prettier-plugin-sort-imports'],
   overrides: [
     {
-      files: "*.astro",
+      files: '*.astro',
       options: {
-        parser: "astro",
+        parser: 'astro',
       },
     },
   ],
