2dc995ef20cfe8e60346199bd1dd68e28f432d3b
krupinskij <jk.krupinski@gmail.com>
2024-08-15 01:03:10 +0200

    Add homepage

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,12 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily: {
+      cursive: ['Great Vibes', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
