03b7c737af28a4a0b6fff52da0672160d612023b
krupinskij <jk.krupinski@gmail.com>
2024-08-16 23:37:29 +0200

    Add styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,8 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      cursive: ['Great Vibes', ...defaultTheme.fontFamily.sans],
+      cursive: ['Italianno', ...defaultTheme.fontFamily.sans],
+      default: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
     },
     extend: {},
   },
