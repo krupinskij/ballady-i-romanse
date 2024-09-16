3728aea8cea3f1e624a8a304775569c403731552
krupinskij <jk.krupinski@gmail.com>
2024-09-16 12:18:33 +0200

    Add site to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://balladyromanse.pl/',
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
