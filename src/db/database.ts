f8e4f892a36f5cd96fb8b25c5a41dae9348303b3
krupinskij <jk.krupinski@gmail.com>
2024-09-18 01:08:41 +0200

    [Test]

--- src/db/database.ts
+++ src/db/database.ts
@@ -3,22 +3,22 @@ import { drizzle } from 'drizzle-orm/libsql';
 
 let _client: ReturnType<typeof createClient> | null = null;
 export const getClient = () => {
-  if (!_client) {
-    _client = createClient({
-      url: process.env.DATABASE_URL || import.meta.env.DATABASE_URL!,
-      authToken: process.env.DATABASE_TOKEN || import.meta.env.DATABASE_TOKEN!,
-    });
-  }
+  // if (!_client) {
+  _client = createClient({
+    url: process.env.DATABASE_URL || import.meta.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_TOKEN || import.meta.env.DATABASE_TOKEN!,
+  });
+  // }
 
   return _client;
 };
 
 let _database: ReturnType<typeof drizzle> | null = null;
 export const getDatabase = () => {
-  if (!_database) {
-    const client = getClient();
-    _database = drizzle(client);
-  }
+  // if (!_database) {
+  const client = getClient();
+  _database = drizzle(client);
+  // }
 
   return _database;
 };
