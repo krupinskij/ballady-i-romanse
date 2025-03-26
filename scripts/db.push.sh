c6148670e781b1d73ed83607164b544bdca9d66c
krupinskij <jk.krupinski@gmail.com>
2025-03-27 00:24:59 +0100

    Update push command

--- scripts/db.push.sh
+++ scripts/db.push.sh
@@ -2,7 +2,7 @@
 
 if [[ $CF_PAGES_BRANCH == "master" ]]; then
   wrangler d1 migrations apply $1-ballady_i_romanse-prod --remote
-elif [[ $CF_PAGES_BRANCH == "develop" ]]
+elif [[ $CF_PAGES_BRANCH == "develop" ]]; then
   wrangler d1 migrations apply $1-ballady_i_romanse-dev --remote
 else
   wrangler d1 migrations apply $1-ballady_i_romanse-dev
