f53aee1b0bab92920b2917c32774c87eb8f41cb2
krupinskij <jk.krupinski@gmail.com>
2025-03-27 00:20:11 +0100

    Update build command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-while getopts ":e:" opt; do
-  case $opt in
-    e) env="$OPTARG"
-    ;;
-    \?) echo "Invalid option -$OPTARG" >&2
-    exit 1
-    ;;
-  esac
-done
-
-if [[ $CF_PAGES_BRANCH == "master" ]]; then
+if [[ $CF_PAGES_BRANCH == "master" || $CF_PAGES_BRANCH == "develop" ]]; then
   bash ./scripts/db.push.sh pl &&
   bash ./scripts/db.push.sh ru &&
   astro check && 
