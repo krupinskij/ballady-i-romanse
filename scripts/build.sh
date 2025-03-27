a4dd7d9eae2a6da064367b50853cefc5ce3ad2fa
krupinskij <jk.krupinski@gmail.com>
2025-03-27 14:09:00 +0100

    Update build command

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-if [[ $CF_PAGES_BRANCH == "master" || $CF_PAGES_BRANCH == "develop" ]]; then
-  bash ./scripts/db.push.sh pl &&
-  bash ./scripts/db.push.sh ru &&
-  astro check && 
-  astro build
-else
-  astro check && 
-  astro build
-fi
+astro check && 
+astro build
