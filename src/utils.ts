9ffa154ceca0464a6def08dfdebfd8008224ffa2
krupinskij <jk.krupinski@gmail.com>
2025-05-02 19:35:15 +0200

    Escape text

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,10 @@
+import { escape } from 'html-escaper';
+
 export const formatText = (
   text: string,
   formatter: (match: string, key: string, title: string) => string
 ) => text.replaceAll(/\{\{(\d*):([^\}]*)\}\}/g, formatter);
+
+export const escapeText = (text: string): string => {
+  return escape(text);
+};
