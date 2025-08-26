f567814e616eadedde22c22f1e3eaadfe1719fc6
krupinskij <jk.krupinski@gmail.com>
2025-08-26 15:45:02 +0200

    Add version

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { sequence } from 'astro:middleware';
 
-import { auth, headers, locals, mobile } from './middlewares';
+import { auth, headers, locals, mobile, version } from './middlewares';
 
-export const onRequest = sequence(headers, locals, auth, mobile);
+export const onRequest = sequence(version, headers, locals, auth, mobile);
